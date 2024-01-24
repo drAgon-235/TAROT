26e81912022c052364e6d8817ad8509d3435a5b3
drAgon-235 <d.slawo@gmail.com>
2024-01-24 20:06:34 +0100

    Quotes TabView completely functional, quote API runniing, favorites Quotes DB running, deleting from favorites is also implemented + nice color Design

---
+++ SpiritualTarot/DailyQuotes/Views/FavoriteQuotesView/OneFQItemView.swift
@@ -0,0 +1,18 @@
+//
+//  OneFQItemView.swift
+//  SpiritualTarot
+//
+//  Created by Slawo Dragon on 24.01.24.
+//
+
+import SwiftUI
+
+struct OneFQItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OneFQItemView()
+}
