ce6317b51f4ba0e149fe1dc9053c98345242990e
drAgon-235 <d.slawo@gmail.com>
2024-01-24 23:02:34 +0100

    After saving the FavoriteQuote the button,that enables you to go to Favorites list within the Sheet View, becomes visible

---
+++ SpiritualTarot/DailyQuotes/Views/Components/ShowFavListBTN.swift
@@ -0,0 +1,18 @@
+//
+//  ShowFavListBTN.swift
+//  SpiritualTarot
+//
+//  Created by Slawo Dragon on 24.01.24.
+//
+
+import SwiftUI
+
+struct ShowFavListBTN: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShowFavListBTN()
+}
