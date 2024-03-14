6a5b7915bd62905f17896c5038172ba9aa74bda0
drAgon-235 <d.slawo@gmail.com>
2024-03-14 19:46:48 +0100

    Login-Workaround: Login Animation + alert if wrong input data + resetting data in input fields

---
+++ SpiritualTarot/Login/Views/Components/LoginAnimTEST.swift
@@ -0,0 +1,18 @@
+//
+//  LoginAnimTEST.swift
+//  SpiritualTarot
+//
+//  Created by Slawo Dragon on 13.03.24.
+//
+
+import SwiftUI
+
+struct LoginAnimTEST: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginAnimTEST()
+}
---
+++ SpiritualTarot/Login/Views/Components/LoginAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  LoginAnimation.swift
+//  SpiritualTarot
+//
+//  Created by Slawo Dragon on 13.03.24.
+//
+
+import SwiftUI
+
+struct LoginAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginAnimation()
+}
